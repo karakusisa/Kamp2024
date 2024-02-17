dd7ab207abd2a938e9300374debe90d6f63a644c
karakusisa <karakusisa@gmail.com>
2024-02-17 13:11:23 +0300

    intro ilk commit

---
+++ Intro/Intro/Business/CourseManager.cs
@@ -0,0 +1,26 @@
+﻿using Intro.DataAccess.Concretes;
+using Intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.Business;
+
+public class CourseManager
+{
+    Course[] cousses = {
+        new Course { Id = 1, Description = "Delphi", Name = "Delphi", Price = 0 }
+    };
+    public CourseManager()
+    {
+
+    }
+    public  List<Course> GetAll()
+    {
+        //Console.WriteLine("GetAll()-> Kurslar listelendi");
+        CourseDal courseDal = new(); 
+        return courseDal.GetAll();
+    }
+}
---
+++ Intro/Intro/DataAccess/Concretes/CourseDal.cs
@@ -0,0 +1,30 @@
+﻿using Intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.DataAccess.Concretes;
+
+public class CorseDal
+{
+    List<Course> courses;
+    public CorseDal()
+    {
+
+        courses = new List<Course>();
+        courses.Add(new Course { Id = 1, Description = "Delphi", Name = "Delphi", Price = 0 });
+
+    }
+    public List<Course> GetAll()
+    {
+        return courses;
+    }
+
+    public void Add(Course course)
+    {
+        courses.Add(course);
+    }
+
+}
